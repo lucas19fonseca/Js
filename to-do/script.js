b9fbdba282ced7e6ef94bcaa319246cb4c63c363
lucas19fonseca <lucas19fonseca@gmail.com>
2025-07-17 18:40:48 -0300

    dab

--- to-do/script.js
+++ to-do/script.js
@@ -1,5 +1,18 @@
-function adcTarefa() {
-    var mensagem = "tarefa adicionada com sucesso!";
-    document.getElementById("mensagem").textContent = mensagem;
-}
+function adcTarefa(){
+    var inputTarefa = document.getElementById("inputTarefa")
+    var tarefa = inputTarefa.value.trim()
 
+    if(tarefa === ""){
+    alert("por favor preencha o campo")
+    return;
+    }
+
+    var ulLista = document.getElementById("ulLista")
+    var novaTarefa = document.createElement("li")
+    ulLista.appendChild(novaTarefa)
+    novaTarefa.textContent = tarefa
+
+    inputTarefa.value = ""
+
+    alert ("tarefa adicionada com sucesso")
+}
