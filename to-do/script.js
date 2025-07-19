2a048f29d793dbba119942dafd4cdaabbd10c42e
lucas19fonseca <lucas19fonseca@gmail.com>
2025-07-19 01:03:05 -0300

    praticando

--- to-do/script.js
+++ to-do/script.js
@@ -1,17 +1,60 @@
+let listaDeTarefas = []
+
 function adcTarefa() {
-    var mensagem = "tarefa adicionada com sucesso!";
-
+    let mensagem = document.getElementById("mensagem")
     let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagem;
-    var listaTarefas = document.getElementById("listaTarefas")
-    var novaTarefa = document.createElement("li")
+    let tarefa = inputTarefa.value.trim()
 
-    novaTarefa.textContent = tarefa
+    if (tarefa === "") {
+        mensagem.textContent = "Campo vazio! Insira algo."
+        mensagem.style.color = "#A34747"
+        return
+    }
+    else {
+        mensagem.textContent = "Tarefa adicionada com sucesso!"
+        mensagem.style.color = "#098f14ff"
+    }
 
-    listaTarefas.appendChild(novaTarefa)
-
-
+    listaDeTarefas.push(tarefa)
+    renderizarTarefa()
 
     inputTarefa.value = ""
+}
+
+function renderizarTarefa() {
+    let listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML = ""
+
+    for (let i = 0; i < listaDeTarefas.length; i++) {
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = listaDeTarefas[i]
+
+
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remover"
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+
+        novaTarefa.appendChild(botaoRemover)
+        novaTarefa.appendChild(botaoEditar)
+        listaTarefas.appendChild(novaTarefa)
+    }
+}
+
+function removerTarefa(i) {
+    listaDeTarefas.splice(i, 1)
+    renderizarTarefa()
+}
+function editarTarefa(i) {
+    let tarefaEditada = prompt("Edite a tarefa:", listaDeTarefas[i])
+    
+    if (tarefaEditada && tarefaEditada.trim() !== "") {
+        listaDeTarefas[i] = tarefaEditada.trim()
+        renderizarTarefa()
+    }
 }
